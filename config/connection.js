a82b5833aeebc7350e56c408cde66d167d325d61
Will <Will.irvin18@gmail.com>
2019-10-28 18:23:37 -0400

    heroku should work

--- config/connection.js
+++ config/connection.js
@@ -12,4 +12,17 @@ connection.connect(function(err) {
     if (err) throw err;
 })
 
+var connection;
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,    
+        user: "root",
+        password: "passroot",
+        database: "burgers_db"
+    });
+}
 module.exports = connection;
