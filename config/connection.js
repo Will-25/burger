3c456846adef11b2d0ac7c0f3abe6d80aeb66e3e
Will <Will.irvin18@gmail.com>
2019-11-13 18:11:38 -0500

    heroky donsky

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,4 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-})
-
-
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
@@ -25,4 +11,7 @@ else {
         database: "burgers_db"
     });
 }
+connection.connect(function(err) {
+    if (err) throw err;
+})
 module.exports = connection;
